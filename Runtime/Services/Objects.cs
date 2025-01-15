9046c0715b77efe2121ef91de0c662702094d47f
Sajad Beigjani <sajad.b@gmail.com>
2025-01-15 13:38:25 +0000

    Update Objects.cs
    removed absolutes.

--- Runtime/Services/Objects.cs
+++ Runtime/Services/Objects.cs
@@ -55,38 +55,5 @@ namespace SeganX.Services
         public string description;
 
         override public string ToString() => $"sku: {sku}, title: {title}, price: {price}, description: {description}";
-
-#if MYKET || BAZAAR || IRGOOGLE || SIPAPP
-        public int ExtractPrice(int defaultValue)
-        {
-            if (string.IsNullOrEmpty(price)) return defaultValue;
-            var label = ConvertPersianDigit(price);
-            string temp = string.Empty;
-            for (int i = 0; i < label.Length; i++)
-                if (char.IsDigit(label[i]) || label[i] == '.')
-                    temp += label[i];
-            var result = temp.ToInt(defaultValue);
-            return price.Contains("ریال") ? result / 10 : result;
-        }
-
-        private string ConvertPersianDigit(string label)
-        {
-            return label
-                .Replace('٠', '0').Replace('١', '1').Replace('٢', '2').Replace('٣', '3').Replace('٤', '4')
-                .Replace('٥', '5').Replace('٦', '6').Replace('٧', '7').Replace('٨', '8').Replace('٩', '9')
-                .Replace('۰', '0').Replace('۱', '1').Replace('۲', '2').Replace('۳', '3').Replace('۴', '4')
-                .Replace('۵', '5').Replace('۶', '6').Replace('۷', '7').Replace('۸', '8').Replace('۹', '9');
-        }
-#else
-        public float ExtractPrice(float defaultValue)
-        {
-            if (string.IsNullOrEmpty(price)) return defaultValue;
-            string temp = string.Empty;
-            for (int i = 0; i < price.Length; i++)
-                if (char.IsDigit(price[i]) || price[i] == '.')
-                    temp += price[i];
-            return temp.ToFloat(defaultValue);
-        }
-#endif
     }
 }
